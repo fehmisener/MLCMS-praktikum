702fdd236bb516eb45055bc5a9557fcb7a7c2c4f
fehmi <fehmiisener@gmail.com>
2024-01-07 18:22:27 +0300

    fix: add cors policy config

--- final-project/src/back-end/app.py
+++ final-project/src/back-end/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from resources import RunScenarioResource
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 api.add_resource(RunScenarioResource, '/run-scenario')
 
--- final-project/src/back-end/resources.py
+++ final-project/src/back-end/resources.py
@@ -10,7 +10,6 @@ class RunScenarioResource(Resource):
             data = RunScenarioInputSchema().load(request.get_json())
             temp_simulation_name = map_to_vadere_scenario(data)
             response = run_scenario(temp_simulation_name)
-
-            return {"message": "Scenario executed successfully", "data": response }, 200
+            return {"message": "Scenario executed successfully", "data": response}, 200
         except Exception as e:
             return {"error": str(e)}, 400
